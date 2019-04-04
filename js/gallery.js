02de29df483a59daf382fb1781d0d37f5dd5e102
foo <killertune8@gmail.com>
2019-04-04 12:21:30 +0900

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,7 @@
 //and respond appropriately on click.
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage = document.querySelector("#gallery-photo > img");
+  let mainImage  = document.querySelector("#gallery-photo > img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function () {
@@ -11,6 +11,17 @@ function activateGallery() {
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", thumbnail.dataset.alt);
+
+      //Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      //Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
